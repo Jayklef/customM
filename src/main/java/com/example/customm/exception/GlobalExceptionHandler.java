e7ba8c6124c5e58eb18d367cff25bda0adc8d33e
Jayklef <jerryarhawho@gmail.com>
2023-03-29 13:02:14 +0100

    Defined GlobalExceptionHandler

---
+++ src/main/java/com/example/customm/exception/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.example.customm.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+}
