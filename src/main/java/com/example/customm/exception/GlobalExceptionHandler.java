488127ed788faaf14f19e59bd96e408925bb4cb7
Jayklef <jerryarhawho@gmail.com>
2023-04-05 16:35:27 +0100

    Added handleMethedArgumentMismatch

--- src/main/java/com/example/customm/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/customm/exception/GlobalExceptionHandler.java
@@ -4,8 +4,11 @@ import com.example.customm.entity.ErrorResponse;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -15,6 +18,10 @@ import org.springframework.web.servlet.resource.HttpResource;
 
 import java.net.URI;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -43,5 +50,23 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
         return new ResponseEntity<>(response, HttpStatus.CONFLICT);
     }
-    
+
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("httpStatus", HttpStatus.BAD_REQUEST.name());
+        body.put("statusCode", HttpStatus.BAD_REQUEST.value());
+        body.put("timeStamp", new Date());
+
+        List<String> errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(e -> e.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        body.put("message", errors);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
 }
